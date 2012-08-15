0ce9544fa7686bae52e8a29c4827899ff720dc74
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2012-08-15 16:27:02 +0300

    remove debug

--- src/Makasim/File/TempFile.php
+++ src/Makasim/File/TempFile.php
@@ -24,7 +24,6 @@ class TempFile extends \SplFileInfo
     public function __destruct()
     {
         if ($this->isFile()) {
-            var_dump('OK');
             @unlink($this->getRealPath());
         }
     }
